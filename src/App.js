4b19beb484aaa118e8c1dee1f470ad1b32e87c53
JaeiKim <dobbykim0320@gmail.com>
2022-10-31 15:17:16 +0900

    dd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,6 @@
 /* eslint-disable no-unused-expressions */
 import React, { useState } from "react";
 
-// function Counter() {
-//   const [number, setNumber] = useState(0);
-
-//   const onIncrease = () => {
-//     setNumber(number + 1);
-//   };
-//   const onDecrease = () => {
-//     setNumber(number - 1);
-//   };
-//   return (
-//     <div>
-//       <h1>{number}</h1>
-//       <button onClick={onIncrease}>+1</button>
-//       <button onClick={onDecrease}>-1</button>
-//     </div>
-//   );
-// }
-
 function Item(props) {
   console.log(props);
 
